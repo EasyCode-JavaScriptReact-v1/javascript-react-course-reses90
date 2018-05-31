d5af77c69cf0cb8fda0d9b9af49bbe18e7da3a66
User <user@Users-MacBook-Pro.local>
2018-05-31 16:59:58 +0300

    homework-1 made by Balashov Nikita. I created object with evry type of data

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,29 @@
+//Home Work 1 made by Balashov Nikita
+
+const firstNumber = 38012345678;
+const secondNumber = 38087654321;
+const thirdNumber = undefined;
+
+const numbers = [firstNumber, secondNumber, thirdNumber];
+
+const photo = null;
+
+const workBlock = {
+    workCompany: "BlaBlaCar",
+    position: "driver",
+    workEmail: "driver@email.com"
+};
+
+
+const user = {
+    photo,
+    title: NaN,
+    firstName: "Ivan",
+    secondName: "Ivanov",
+    numbers,
+    workBlock,
+    inViber: true,
+    inTelegram: false,
+};
+
+console.log(user);
