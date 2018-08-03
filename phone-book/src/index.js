3e644a574300fe3cffbd3cd8da231cadcc660adf
Balashov Nikita <reses142@gmail.com>
2018-08-03 18:25:59 +0300

    call mode almost

--- phone-book/src/index.js
+++ phone-book/src/index.js
@@ -6,7 +6,7 @@ class App {
         this.store = this.createStore();
         this.pages = {
            'contacts': new ContactPage(this.store),
-           'keypad': new KeypadPage(this.store),
+           'keypad': new KeypadPage(this.store, this.applyListenerForNavigation),
         }
         this.pages.contacts.setStateContact();
         this.render();
@@ -134,6 +134,8 @@ class App {
     }
 }
 
+/* ================== CONTACT START================== */
+
 class ContactPage {
     constructor(store) {
         this.setStateContact = () => {
@@ -144,6 +146,7 @@ class ContactPage {
             };
             setState(initializeState);
         }
+
     }
 
     render() {
@@ -243,6 +246,9 @@ class ContactPage {
     }
 }
 
+/* ================== CONTACT END================== */
+/* ================== KEYPAD START================== */
+
 class KeypadPage {
     constructor(store) {
         this.setStateKeypad = () => {
@@ -253,6 +259,8 @@ class KeypadPage {
             };
             setState(initializeState);
         }
+
+        this.callMode = new CallMode();
     }
 
     render() {
@@ -312,6 +320,7 @@ class KeypadPage {
         /* ADD NUMBER */
         const keypadButtons = document.getElementById('wraper-for-buttons');
         const fieldForNumber = document.getElementById('number');
+        const fieldForOperator = document.getElementById('operator');
 
         keypadButtons.addEventListener('click', (e) => {
             const TARGET_CLASS_NAME = e.target.className;
@@ -319,6 +328,13 @@ class KeypadPage {
             if(TARGET_CLASS_NAME === 'number-btn') {
                 const BUTTON_VALUE = e.target.textContent;
                 fieldForNumber.textContent += BUTTON_VALUE;
+
+                const LENGTH_OF_NUMBERS = fieldForNumber.textContent.length;
+
+                if(LENGTH_OF_NUMBERS === 3) {
+                    fieldForOperator.textContent = this.indentifyMobileOperator(fieldForNumber.textContent)
+                }
+
             }
         });
 
@@ -335,13 +351,177 @@ class KeypadPage {
                 || BUTTON_ID_FROM_SVG === 'delete'
                 || BUTTON_ID_FROM_PATH === 'delete'
             ) {
-                const LENGTH_OF_NUMBERS = fieldForNumber.textContent.length - 1;
-                const strWithDeletedOneNumber = fieldForNumber.textContent.slice(0, LENGTH_OF_NUMBERS)
+                const LENGTH_OF_NUMBERS = fieldForNumber.textContent.length;
+                const LENGTH_OF_NUMBERS_FOR_SLICE = fieldForNumber.textContent.length - 1;
+                const strWithDeletedOneNumber = fieldForNumber.textContent.slice(0, LENGTH_OF_NUMBERS_FOR_SLICE)
                 fieldForNumber.textContent = strWithDeletedOneNumber;
+
+                if(LENGTH_OF_NUMBERS <= 3) {
+                    fieldForOperator.textContent = '';
+                }
+
                 return;
             }
         });
+
+        /* TO CALL BUTTON */
+        const mainWraper = document.getElementById('main-wraper');
+
+        const toCallButton = document.getElementById('call-btn');
+        toCallButton.addEventListener('click', () => {
+            const ERROR_BLOCK = document.getElementById('error-block');
+
+            if(fieldForNumber.textContent.length < 3) {
+                ERROR_BLOCK.textContent = `ERROR you've typed incorrect number`;
+                setTimeout(() => {
+                    ERROR_BLOCK.textContent = '';
+                }, 3000);
+                return;
+            }
+
+            const NUMBER_FOR_CALL_MODE = this.callMode.getNumber();
+            const OPERATOR_FOR_CALL_MODE = this.callMode.getOperator();
+
+            mainWraper.classList.add('call-mode');
+            mainWraper.innerHTML = this.callMode.render(NUMBER_FOR_CALL_MODE, OPERATOR_FOR_CALL_MODE);
+            this.callMode.applyListenerForCallMode();
+        })
+    }
+
+    indentifyMobileOperator(firstThreeNumbers) {
+        let operator;
+
+        MOBILE_OPERATORS_IDENTIFICATORS.kuivstar.forEach(identicationNumber => {
+            if(identicationNumber === firstThreeNumbers) {
+                operator = 'Kuivstar'
+            }
+        })        
+
+        MOBILE_OPERATORS_IDENTIFICATORS.life.forEach(identicationNumber => {
+            if(identicationNumber === firstThreeNumbers) {
+                operator = 'Life'
+            }
+        })   
+
+        MOBILE_OPERATORS_IDENTIFICATORS.vodafone.forEach(identicationNumber => {
+            if(identicationNumber === firstThreeNumbers) {
+                operator = 'Vodafone'
+            }
+        })   
+
+        return operator;
     }
 }
+
+/* ================== KEYPAD END================== */
+/* ================== CALL MODE START================== */
+class CallMode{
+    constructor() {}
+
+    render(number, operator) {
+        return /*html*/`
+        <div id="callMode">
+            <header class="container">
+
+                <div class="row d-flex justify-content-center">
+                    <span class="call-target">${number}</span>
+                    <span class="phone-code">${operator}</span>
+                </div>
+
+                <div class="row d-flex justify-content-center">
+                    <span id="load-point-1" class="load-point fas fa-circle"></span>
+                    <span id="load-point-2" class="load-point fas fa-circle"></span>
+                    <span id="load-point-3" class="load-point fas fa-circle"></span>
+                    <span id="load-point-4" class="load-point fas fa-circle"></span>
+                    <span id="load-point-5" class="load-point fas fa-circle"></span>
+                </div>
+
+            </header>
+
+            <main class="all-mode-keypad-block">
+
+                <div class="wraper-for-call-mode-buttons">
+
+                    <div class="row">
+
+                        <div class="wraper-for-call-mode-btn">
+                            <div class="call-mode-btn">
+                                <span class="fas fa-microphone-alt-slash"></span>
+                            </div>
+                            <span class="call-btn-description">mute</span>
+                        </div>
+
+                        <div class="wraper-for-call-mode-btn">
+                            <div class="call-mode-btn">
+                                <span class="fas fa-th"></span>
+                            </div>
+                            <span class="call-btn-description">keypad</span>
+                        </div>
+                        
+                        <div class="wraper-for-call-mode-btn">
+                            <div class="call-mode-btn">
+                                <span class="fas fa-volume-up"></span>
+                            </div>  
+                            <span class="call-btn-description">volume</span>
+                        </div>
+                        
+                    </div>
+
+                    <div class="row">
+                        <div class="wraper-for-call-mode-btn">
+                            <div class="call-mode-btn">
+                                <span class="fas fas fa-plus"></span>
+                            </div>  
+                            <span class="call-btn-description">add</span>
+                        </div>
+                        
+                        <div class="wraper-for-call-mode-btn">
+                            <div class="call-mode-btn">
+                                <span class="fas fa-video"></span>
+                            </div>  
+                            <span class="call-btn-description">video</span>
+                        </div>
+
+                        <div class="wraper-for-call-mode-btn">
+                            <div class="call-mode-btn">
+                                <span class="fas fa-users"></span>
+                            </div>  
+                            <span class="call-btn-description">contacts</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row d-flex justify-content-center">
+                    <div id="call-off" class="call-mode-btn call-off">
+                        <span class="fas fa-phone"></span>
+                    </div>  
+                </div>
+
+            </main>
+        </div>
+        `;
+    }
+
+    getNumber() {
+        const phoneNumber = document.getElementById('number');
+        return phoneNumber.textContent;
+    }
+
+    getOperator() {
+        const phoneOperator = document.getElementById('operator');
+        return phoneOperator.textContent;
+    }
+
+    applyListenerForCallMode() {
+        
+    }
+
+    setSavedKeypadPage(page) {
+        this.savedKeypadPage = page;
+    }
+}
+
+/* ================== CALL MODE END================== */
 
 const APPLICATION = new App();
