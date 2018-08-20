c525e3ae47b5fb1d4acb4668a2391de8cb44ef8b
Balashov Nikita <reses142@gmail.com>
2018-08-20 20:15:06 +0300

    add-user-page

--- phone-book/src/add-user/add-user.js
+++ phone-book/src/add-user/add-user.js
@@ -1,7 +1,7 @@
 import {Url} from '../url/url';
 
 class AddUserPage {
-    constructor(store) {
+    constructor(store, accountName) {
         this.setStateContact = () => {
             const {setState} = store;
             const initializeState = {
@@ -11,7 +11,7 @@ class AddUserPage {
             setState(initializeState);
         }
 
-        this.serverSide = new Url();
+        this.url = new Url(accountName);
     }
 
     render() {
@@ -28,17 +28,17 @@ class AddUserPage {
                     <form>
 
                         <div class="form-group">
-                            <label for="fullName-input">Full name:</label>
+                            <label for="fullName-input"><span class="required">*</span>Full name:</label>
                             <input type="text" name="fullName" class="form-control" id="fullName-input" placeholder="Enter full name" required>
                         </div>
 
                         <div class="form-group">
-                            <label for="email-input">Email address:</label>
+                            <label for="email-input"><span class="required">*</span>Email address:</label>
                             <input type="email" name="email" class="form-control" id="email-input" placeholder="Enter email" required>
                         </div>
 
                         <div class="form-group">
-                            <label for="phoneNumber-input">Phone Number:</label>
+                            <label for="phoneNumber-input"><span class="required">*</span>Phone Number:</label>
                             <input type="number" name="phone" class="form-control" id="phoneNumber-input" placeholder="Enter phone number" required>
                         </div>
 
@@ -78,40 +78,193 @@ class AddUserPage {
     }
 
     applyListenersForAddUserPage() {
-        // const fullNameInput = document.querySelector('#fullName-input');
-        // const emailInput = document.querySelector('#email-input');
-        // const phoneNumberInput = document.querySelector('#phoneNumber-input');
-        // const birthDateInput = document.querySelector('#birthDate-input');
-        // const addressInput = document.querySelector('#address-input');
-        // const genderInput = document.querySelector('#gender-selection');
-        // const avatarUrlInput = document.querySelector('#avatarUrl-input');
 
         const addUserForm = document.querySelector('form');
+
+        const handlerForInputs = (e) => {
+            if(e.target.name === 'fullName') {
+                const VALUE = e.target.value;
+
+                if(VALUE.length === 0) {
+                    e.target.classList.remove('wrong')
+                    e.target.classList.remove('correct')
+                    return;
+                }
+        
+                if(this.isValidFullName(VALUE)) {
+                    e.target.classList.add('correct')
+                    e.target.classList.remove('wrong')
+                } else {
+                    e.target.classList.add('wrong')
+                    e.target.classList.remove('correct')
+                }     
+            }
+
+            if(e.target.name === 'email') {
+                const VALUE = e.target.value;
+
+                if(VALUE.length === 0) {
+                    e.target.classList.remove('wrong')
+                    e.target.classList.remove('correct')
+                    return;
+                }
+
+                if(this.isValidEmail(VALUE)) {
+                    e.target.classList.add('correct')
+                    e.target.classList.remove('wrong')
+                } else {
+                    e.target.classList.add('wrong')
+                    e.target.classList.remove('correct')
+                }
+            }
+
+            if(e.target.name === 'phone') {
+                const VALUE = e.target.value;
+
+                if(VALUE.length === 0) {
+                    e.target.classList.remove('wrong')
+                    e.target.classList.remove('correct')
+                    return;
+                }
+
+                if(VALUE.length > 9) {
+                    e.target.classList.add('correct')
+                    e.target.classList.remove('wrong')
+                } else {
+                    e.target.classList.add('wrong')
+                    e.target.classList.remove('correct')
+                }
+            }
+
+            if(e.target.name === 'birthdate') {
+                const VALUE = e.target.value;
+
+                if(VALUE.length === 0) {
+                    e.target.classList.remove('correct')
+                    return;
+                }
+
+                if(VALUE.length > 9) {
+                    e.target.classList.add('correct')
+                }
+            }
+
+            if(e.target.name === 'address') {
+                const VALUE = e.target.value;
+
+                if(VALUE.length === 0) {
+                    e.target.classList.remove('correct')
+                    return;
+                }
+
+                if(VALUE.length > 0) {
+                    e.target.classList.add('correct')
+                }
+            }
+
+            if(e.target.name === "avatarUrl") {
+                const VALUE = e.target.value;
+
+                if(VALUE.length === 0) {
+                    e.target.classList.remove('wrong')
+                    e.target.classList.remove('correct')
+                    return;
+                }
+
+                if(this.isValidURL(VALUE)) {
+                    e.target.classList.add('correct')
+                    e.target.classList.remove('wrong')
+                } else {
+                    e.target.classList.add('wrong')
+                    e.target.classList.remove('correct')
+                }
+            }
+            
+        };
+
+        addUserForm.addEventListener('input', handlerForInputs)
+
         const inputs = [...addUserForm.elements]
                             .filter(elem => elem.tagName === 'INPUT' || elem.tagName === 'SELECT');
 
-        addUserForm.addEventListener('submit', (e) => {
+        const handlerForSubmit = (e) => {
+            e.preventDefault();
 
             const user = inputs.reduce((newUser, input) => {
-                const KEY = input.name;
-                const VALUE = input.value;
-
-                if(VALUE.lenght !== 0) {
-                    newUser[KEY] = VALUE;
+                if(input.classList.contains('wrong')) {
+                    alert(`${input.name} is incorrect!`);
+                    return;
+                };
+                if(input.value.length !== 0 
+                    && input.name !== 'phone' 
+                    && input.name !== 'gender'
+                    && input.name !== 'fullName'
+                ) {
+                    newUser[input.name] = input.value;
                 }
-                if(VALUE === 'Male' || VALUE === 'Female') {
-                    const firstChar = input.value[0];
-                    newUser[KEY] = firstChar;
+                if(input.value.length !== 0 && input.name === 'phone') {
+                    newUser[input.name] = input.value.replace(/(.{3})(.{3})(.{2})/g, '($1) $2-$3-');
+                }
+                if(input.name === 'gender') {
+                    input.value === "Male"
+                        ? newUser[input.name] = "M"
+                        : newUser[input.name] = "F"
+                }
+                if(input.value.length !== 0 && input.name === 'fullName') {
+                    const formatedFullName = input.value.split(' ').reduce((output, word, index) => {
+                        const splitedWord = word.toLowerCase().split('');
+                        const firstLetter = splitedWord[0].toUpperCase();
+                        splitedWord[0] = firstLetter;
+                        output += splitedWord.join('');
+
+                        if(index === 0) {
+                            output += ' ';
+                        }
+
+                        return output;
+                    }, '');
+
+                    newUser[input.name] = formatedFullName;
                 }
                 return newUser;
-            }, {});
-            
-            this.serverSide.postUser(user);
-        })
+            }, {})
+
+            if(user) {
+                this.url.postUser(user);
+
+                inputs.forEach(input => {
+                    if(input.tagName !== "SELECT") {
+                        input.value = "";
+                        input.classList.remove('correct');
+                    }
+                })
+            } else {
+                alert('Something is incorrect!')
+            }
+        }
+
+        addUserForm.addEventListener('submit', handlerForSubmit)
     }
 
-    isValid() {
+    isValidFullName(value) {
+        const splitedValue = value.split(' ');
 
+        return splitedValue.length === 2 && splitedValue[0].length > 0 && splitedValue[1].length > 0;
+    }
+
+    isValidEmail(value) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(value).toLowerCase());  
+    }
+
+    isValidURL(value) {
+        const re = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return re.test(String(value).toLowerCase());
     }
 }
 
